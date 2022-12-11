cc6d77547c9632b886546353a03dcdcbbc9d8384
Volodymyr <mospan.volodymyr@gmail.com>
2022-12-11 18:45:28 +0200

    Completed task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,30 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRef = document.querySelector('div.gallery');
+
+galleryRef.innerHTML = createGalleryMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox(galleryRef.children, {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        />
+        </a>`
+    )
+    .join('');
+}
