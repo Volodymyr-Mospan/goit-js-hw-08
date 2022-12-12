c08d4adc846368bc8982491cf07370a5d1a6fecb
Volodymyr <mospan.volodymyr@gmail.com>
2022-12-12 19:58:47 +0200

    working on task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
 
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', function (timeupdate) {
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(timeupdate));
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+};
